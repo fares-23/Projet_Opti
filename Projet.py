a9c8898eafd51f36fafebb63a672aa916c7decfa
NKevinVI <kevinnoche666@gmail.com>
2025-01-09 20:44:20 +0100

    MÀJ Simulation, NSGA-II et Monte-Carlo.

--- Projet.py
+++ Projet.py
@@ -6,9 +6,9 @@ import random
 # IMPORTANT: Les unités sont toutes en SI, sauf indication contraire explicite.
 
 
-def Simulation(Bat_cap, Train_Seuil):
+def Simulation(Bat_cap,seuil):
     """
-        Simulation d'un cas particulier, pour une capacité de batterie Bat_cap connue et un seuil de demande d'énergie à la batterie Train_Seuil donnée également.
+        Simulation d'un cas particulier, pour une capacité de batterie Bat_cap connue et un seuil de demande d'énergie à la batterie seuil donnée également.
     """
 
     """
@@ -31,9 +31,7 @@ def Simulation(Bat_cap, Train_Seuil):
     rho_rail = 10*10**(-6) # Résistance linéique des rails.
     x_Tot = 5000 # Distance totale que le train doit parcourir durant son trajet.
     SysBor = 35000 # Consommation du système de bord.
-    rend = 0.8 # Rendement du moteur+convertisseurs.
-
-
+    rend = 0.8 # Rendement du moteur+convertisseurs
 
     """
         GRANDEURS MESURÉES
@@ -43,8 +41,6 @@ def Simulation(Bat_cap, Train_Seuil):
     marche = np.loadtxt("marche.txt")
     t = marche[:, 0] # Temps.
     x = marche[:, 1] # Distance parcourue par le train.
-
-
 
     """
         TRAIN
@@ -75,43 +71,36 @@ def Simulation(Bat_cap, Train_Seuil):
         else:
             P_train[k] = P_mecanique[k]*rend+SysBor # On produit l'énergie.
 
-
-
     """
         BATTERIE
         ========
     """
-
-    # Bat_cap = 100000 # Capacité de la batterie (Wh).
     Bat_rend = 0.9 # Rendement de la batterie.
     Bat_E = np.zeros(len(P_train)) # Énergie contenue dans la batterie (à un instant t).
     Bat_Charge = np.zeros(len(P_train)) # Charge de la batterie, au cours du temps.
     Bat_Charge[0] = 0 # Charge de départ de la batterie.
     if Bat_Charge[0] > Bat_cap*3600 and Bat_Charge < 0: # On vérifie qu'on ne dépasse pas le capacité de la batterie.
         raise ValueError("On dépasse les capacités de la batterie!")
-    # Train_Seuil = 350000 # Seuil de puissance (négatif, en joule) auquel le train demande de l'énergie.
     Rheo_P = np.zeros(len(P_train)) # Puissance dissipée par le rhéostat.
     Rheo_P[0] = 0 # Le rhéostat ne dissipe rien au départ.
 
     for k in range(1, len(t)):
         if P_train[k] < 0: # Le train freine.
-            Bat_E[k] = Bat_E[k-1]-P_train[k]*Bat_rend
+            Bat_E[k] = Bat_E[k-1]-P_train[k] * Bat_rend
             P_train[k] = 0
             if Bat_E[k] > 3600*Bat_cap: # Dépasse-t-on les limites de la batterie?
                 Diff = Bat_E[k] - Bat_cap*3600
                 Bat_E[k] = Bat_cap*3600
                 Rheo_P[k] = Diff # On dissipe dans le rhéostat.
-        elif P_train[k] >= Train_Seuil: # Si le train demande de l'énergie.
-            Bat_E[k] = Bat_E[k-1] - (P_train[k]-Train_Seuil)
-            P_train[k] = P_train[k] - (P_train[k]-Train_Seuil) * Bat_rend
+        elif P_train[k] >= seuil: # Si le train demande de l'énergie.
+            Bat_E[k] = Bat_E[k-1] - (P_train[k]-seuil)
+            P_train[k] = P_train[k] - seuil * Bat_rend
             if Bat_E[k] < 0: # Dépasse-t-on les limites de la batterie?
                 Diff = -Bat_E[k]
                 Bat_E[k] = 0
                 P_train[k] += Diff
         else:
             Bat_E[k] = Bat_E[k-1] # La batterie conserve son énergie.
-
-
 
     """
         RÉSEAU FERROVIAIRE
@@ -128,7 +117,7 @@ def Simulation(Bat_cap, Train_Seuil):
     P_LAC = np.zeros(len(P_train))
     for k in range(len(P_train)):
         if P_train[k] > (V_SST**2)/(4*R_eq[k]):
-            P_LAC[k] = (V_SST**2)/(4*R_eq[k])
+            P_LAC[k] = (V_SST**2)/(4*R_eq[k])-SysBor
         else:
             P_LAC[k] = P_train[k]
 
@@ -154,143 +143,8 @@ def Simulation(Bat_cap, Train_Seuil):
     Ind_qual = V_SST - np.min(V_train)
     IndCrit = V_SST - 500 # Valeur critique du Ind_qual, à ne pas dépasser.
 
-
-
-    """
-        AFFICHAGE
-        =========
-    """
-
-    # plt.figure("Position, vitesse, accélération du train en fonction du temps")
-    # # Affichage position :
-    # plt.subplot(3, 1, 1)
-    # plt.plot(t, x/1000, "-k", label="Position du train") # Position normalisée en km.
-    # plt.title("Position, vitesse, accélération du train en fonction du temps")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Longueur [km]")
-    # plt.grid()
-    # plt.legend()
-    #
-    # # Affichage vitesse :
-    # plt.subplot(3, 1, 2)
-    # plt.plot(t, v/1000, "-k", label="Vitesse du train") # Vitesse normalisée en km/s.
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Vitesse [km/s]")
-    # plt.grid()
-    # plt.legend()
-    # # Affichage accélération :
-    # plt.subplot(3, 1, 3)
-    # plt.plot(t, a/g, "-k", label="Accélération du train") # Accélération normalisée en g.
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Accélération [g]")
-    # plt.grid()
-    # plt.legend()
-    #
-    #
-    # plt.figure("Puissance, tension et courant dans le train")
-    # # Affichage de la puissance :
-    # plt.subplot(3, 1, 1)
-    # plt.plot(t, P_train/1000000, "-k", label="Puissance consommée par le train") # P_train normalisée en MW.
-    # plt.title("Puissance, tension et courant dans le train")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Puissance [MW]")
-    # plt.legend()
-    # plt.grid()
-    #
-    # # Affichage de la tension:
-    # plt.subplot(3, 1, 2)
-    # plt.plot(t, V_train, "-k", label="Tensions aux bornes de la locomotive")
-    # plt.legend()
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Tension [V]")
-    # plt.grid()
-    #
-    # # Affichage du courant:
-    # plt.subplot(3, 1, 3)
-    # plt.plot(t, I_train, "-k", label="Courant traversant le train")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Courant [A]")
-    # plt.legend()
-    # plt.grid()
-    #
-    #
-    # plt.figure("Courants dans les branches du circuit")
-    # # Affichage de I_1, I_2 et I_train:
-    # plt.plot(t, I_1, "-b", label="I_1")
-    # plt.plot(t, I_2, "-g", label="I_2")
-    # plt.plot(t, I_train, "-k", label="I_train")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Courant [A]")
-    # plt.title("Courants dans les branches du circuit")
-    # plt.grid()
-    # plt.legend()
-    #
-    #
-    # plt.figure("Puissances mises en jeu")
-    # # Affichage Puissance des stations et du train.
-    # plt.subplot(3, 1, 1)
-    # plt.plot(t, P_SST1/1000000, "-b", label="Sous-station 1") # normalisé en MW
-    # plt.plot(t, P_SST2/1000000, "-g", label="Sous-station 2") # normalisé en MW
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Puissance [MW]")
-    # plt.title("Puissances mises en jeu")
-    # plt.legend()
-    # plt.grid()
-    #
-    # # Affichage de la puissance des stations avec pertes.
-    # plt.subplot(3, 1, 2)
-    # plt.plot(t, P_SST1_loss/1000000, "-m", label="Perte 1") # normalisé en MW
-    # plt.plot(t, P_SST2_loss/1000000, "-c", label="Perte 2") # normalisé en MW
-    # plt.grid()
-    # plt.legend()
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Puissance [MW]")
-    #
-    # # Affichage de la puissance des stations avec pertes.
-    # plt.subplot(3, 1, 3)
-    # plt.plot(t, (P_SST1+P_SST2-P_SST1_loss-P_SST2_loss)/1000000, "-r", label="Puissance des stations, avec pertes") # normalisé en MW
-    # plt.plot(t, P_train/1000000, "-k", label="Puissance consommée par le train") # P_train normalisé en MW
-    # plt.grid()
-    # plt.legend()
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Puissance [MW]")
-    #
-    #
-    # plt.figure("Gestion de la Batterie")
-    # # Affichage de l'énergie de la batterie, de la consommation du rhéostat et de l'énergie du train ainsi que de sa tension.
-    # plt.subplot(3, 1, 1)
-    # plt.plot(t, Bat_E/1000000, "-b", label="Énergie dans la batterie")
-    # plt.plot(t, Rheo_P/1000000, "-g", label="Énergie perdue dans le rhéostat")
-    # plt.title("Gestion de la Batterie")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Énergie [MJ]")
-    # plt.grid()
-    # plt.legend()
-    #
-    # plt.subplot(3, 1, 2)
-    # plt.plot(t, P_train/1000000, "-k", label="Puissance consommée par le train")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Puissance [MW]")
-    # plt.grid()
-    # plt.legend()
-    #
-    # plt.subplot(3, 1, 3)
-    # plt.plot(t, V_train, "-k", label="Tension aux bornes du train")
-    # plt.xlabel("Temps [s]")
-    # plt.ylabel("Tension [V]")
-    # plt.grid()
-    # plt.legend()
-    #
-    #
-    # plt.figure("Indication de la Qualité du Système")
-    # # Comparaison, dans un histogramme, de Ind_qual et IndCrit.
-    # plt.bar(["Indicateur Critique\n(à ne pas dépasser)", "Indicateur Actuelle"], [IndCrit, Ind_qual], color="grey", edgecolor="black")
-    # plt.title("Indication de la Qualité du Système")
-    # plt.ylabel("Différence de Tension [V]")
-    # plt.grid()
-    # plt.legend()
-
     return Ind_qual # Indicateur de qualité = Chute de tension maximale.
+
 
 
 def non_dominant_sort(pop):
@@ -317,6 +171,30 @@ def non_dominant_sort(pop):
         front = [] # On réinitialise le premier rang.
     return fronts
 
+
+
+def MonteCarlo():
+    capacites = np.linspace(0, 14000, 1000)  # Capacité batterie en kWh
+    seuils = np.linspace(0, 1e6, 1000)  # Seuil de puissance (W)
+
+    # Tirer x couples aléatoires pour l'espace des solutions
+    couples_aleatoires = [(random.choice(capacites), random.choice(seuils)) for _ in range(1000)]
+    solutions = []
+
+    for capacite, seuil in couples_aleatoires:
+        chute_tension_max= Simulation(capacite,seuil)
+        solutions.append((capacite, seuil, chute_tension_max))
+
+    # capacites, seuils, chute_tension_max_values = zip(*solutions)
+    # plt.figure(figsize=(6, 4))
+    # plt.scatter(capacites, chute_tension_max_values)  # Scatter plot de l'espace des solutions
+    # plt.title("Espace des solutions", fontsize=14)
+    # plt.xlabel("Capacité de la batterie (kWh)", fontsize=12)
+    # plt.ylabel("Chute tension (V)", fontsize=12)
+    # plt.grid(True)
+    # plt.show()
+
+    return solutions
 
 
 
@@ -448,6 +326,6 @@ def NSGA2(CapaLim, CapaStep, SeuilLim, SeuilStep, PopSize, N, mutant=0.25):
     =======
 """
 
-NSGA2([1000, 10000], 1000, [0, 100000], 10000, 25, 50)
+NSGA2([1000, 10000], 1000, [0, 100000], 10000, 10, 20)
 
 plt.show()
